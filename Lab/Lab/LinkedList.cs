a2a5ce2826aaecace8f4eb2391770374f7fd55be
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-13 21:01:42 +0300

    Added method Delete in the class LinkedList

--- Lab/Lab/LinkedList.cs
+++ Lab/Lab/LinkedList.cs
@@ -17,6 +17,35 @@ namespace Lab1
 			Count = 0;
 		}
 
+		public void Delete(T data)
+		{
+			if (First == null)
+				return;
+
+			if(First.Data.Equals(data))
+			{
+				First = First.Next;
+				Count--;
+				return;
+			}
+
+			var current = First.Next;
+			var previous = First;
+
+			while(current != null)
+			{
+				if(current.Data.Equals(data))
+				{
+					previous.Next = current.Next;
+					Count--;
+					return;
+				}
+
+				previous = current;
+				current = current.Next;
+			}
+		}
+
 		public void Add(T data)
 		{
 			var item = new Item<T>(data);
--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -11,6 +11,16 @@ namespace Lab
 			linkedList.Add(11);
 			linkedList.Add(22);
 			linkedList.Add(333);
+			linkedList.Add(1);
+			linkedList.Add(1232);
+			linkedList.Add(123234);
+			linkedList.Add(435);
+
+			linkedList.Delete(11);
+			linkedList.Delete(333);
+			linkedList.Delete(435);
+			linkedList.Delete(11111);
+
 			Console.ReadLine();
 		}
 	}

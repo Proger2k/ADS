af15317a0b82d7c8cfa9e80dbf72373dd5b253a4
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-14 11:18:21 +0300

    Added tests

--- Lab/Lab/Program.cs
+++ Lab/Lab/Program.cs
@@ -9,26 +9,38 @@ namespace Lab
 		{
 			var linkedList = new LinkedList<int>();
 
+			Console.Write("Элементы списка: ");
 			linkedList.PrintLinkedList();
-			Console.WriteLine(linkedList.IsEmpty());
+			Console.WriteLine("Пустой ли список ? " + linkedList.IsEmpty());
+			Console.WriteLine("Кол-во элементов в списке: " + linkedList.Count);
+			Console.WriteLine("Есть ли число 11 в списке ? " + linkedList.Search(11));
+			Console.WriteLine("Добавление элементов");
+			Console.WriteLine();
 
 			linkedList.Add(10);
 			linkedList.Add(2);
-			Console.WriteLine("Найти 2 : " + linkedList.Search(2));
 			linkedList.Add(22);
 			linkedList.Add(11);
-			Console.WriteLine("Найти 11 : " + linkedList.Search(11));
+
+			Console.Write("Элементы списка: ");
 			linkedList.PrintLinkedList();
-			Console.WriteLine(linkedList.IsEmpty());
+			Console.WriteLine("Пустой ли список ? " + linkedList.IsEmpty());
+			Console.WriteLine("Кол-во элементов в списке: " + linkedList.Count);
+			Console.WriteLine("Есть ли число 11 в списке ? " + linkedList.Search(11));
+			Console.WriteLine("Есть ли число 12 в списке ? " + linkedList.Search(12));
+			Console.WriteLine("Удаление элементов");
+			Console.WriteLine();
 
 			linkedList.Delete(10);
 			linkedList.Delete(2);
-			Console.WriteLine("Найти 2 : " + linkedList.Search(2));
 			linkedList.Delete(22);
 			linkedList.Delete(11);
 
+			Console.Write("Элементы списка: ");
 			linkedList.PrintLinkedList();
-			Console.WriteLine(linkedList.IsEmpty());
+			Console.WriteLine("Пустой ли список ? " + linkedList.IsEmpty());
+			Console.WriteLine("Кол-во элементов в списке: " + linkedList.Count);
+			Console.WriteLine("Есть ли число 11 в списке ? " + linkedList.Search(11));
 
 			Console.ReadLine();
 		}

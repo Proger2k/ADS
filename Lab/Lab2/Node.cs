c8341004e28a383b97e99a338343d3ac43eba27e
Proger2k <stolbetskiy.yura@gmail.com>
2021-04-02 14:48:19 +0300

    Added class Node

---
+++ Lab/Lab2/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab2
+{
+    public class Node<T>
+    {
+        private T Data { get; set; }
+        private Node<T> Parent { get; set; }
+        private Node<T> LeftChild { get; set; }
+        private Node<T> RightChild { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return $"{Data}"; 
+        }
+    }
+}
